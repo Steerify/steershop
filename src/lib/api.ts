3737dfb3c599f16c217601f3a0f55db0c29a8587
Kingvic <oladimejivictor611@gmail.com>
2026-01-02 09:54:08 +0100

    fixing the google oauth

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,8 @@
 // src/lib/api.ts
 import axios from 'axios';
 
-// const API_BASE_URL = 'https://steershop-backend.onrender.com/api/v1';
-const API_BASE_URL = 'http://localhost:5000/api/v1';
+const API_BASE_URL = 'https://steershop-backend.onrender.com/api/v1';
+// const API_BASE_URL = 'http://localhost:5000/api/v1';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
